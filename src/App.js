12c521a09a480ab80874f9ec947f4b0309786041
Nicholas Wease <nwease@Nicholass-MacBook-Air.local>
2019-11-18 12:19:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Link} from 'react-router-dom'
+import './App.css'
+import Container from 'react-bootstrap/Container';
+import {Navbar} from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: 'Nicholas Wease',
+            headerLinks: [
+                { title: 'Home', path: '/' },
+                { title: 'About', path: '/about' },
+                { title: 'Contact', path: '/contact' }
+            ],
+
+            home: {
+                title: 'Be Powerful',
+                subTitle: 'Projects that Matter',
+                text: 'Some of my work'
+            },
+
+            about: {
+                title: 'About Me'
+            },
+
+            contact: {
+                title: 'Lets Talk About the Possibilities'
+            }
+        }
+    }
+
+    render() {
+        return (
+            <Router>
+                <Container className='p-0' fluid={true}>
+                    <Navbar className='border-bottom' bg='transparent' expand='lg'>
+                        <Navbar.Brand>
+                            Nicholas Wease
+                        </Navbar.Brand>
+
+                        <Navbar.Toggle className='border-0' aria-controls='navBar-toggle' />
+
+                        <Navbar.Collapse id='navBar-toggle'>
+                            <Nav className='ml-auto'>
+                                <Link className='nav-link' to='/'>
+                                    Home
+                                </Link>
+
+                                <Link className='nav-link' to='/about'>
+                                    About
+                                </Link>
+
+                                <Link className='nav-link' to='/contact'>
+                                    Contact
+                                </Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </Container>
+            </Router>
+        );
+    }
 }
 
 export default App;
